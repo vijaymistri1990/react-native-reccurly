f2a16610cbf6f99796658443c52a22f88d34a569
vijaymistri1990 <vijaymakwana12790@gmail.com>
2026-09-19 18:35:06 +0530

    add style sheet and fixed the tab issue

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,13 +1,39 @@
 import { Tabs } from "expo-router";
+import { View } from "react-native";
+import clsx from 'clsx'
+import { Image } from "expo-image";
+import { tabs } from "../../../constants/data";
 
-const TabLayout = ()=>(
-    <Tabs screenOptions={{headerShown:false}}>
-        <Tabs.Screen name="index" options={{title:"Home"}} />
-        <Tabs.Screen name="Inisite" options={{title:"insights"}} />
-        <Tabs.Screen name="Settings" options={{title:"settings"}} />
-        <Tabs.Screen name="Subscription" options={{title:"subscriptions"}} />
-        <Tabs.Screen name="subscription/[id]" options={{href:null}} />
-    </Tabs>
-)
+const TabIcon = ({ focused, icon }: TabIconProps) => {
+    return (
+        <View className="tabs-icon">
+            <View className={clsx('tabs-pill', focused && 'tabs-active')}> 
+                <Image className="tabs-glyph" source={icon} />
+            </View>
+        </View>
+    )
+}
+
+const TabLayout = () => {
+    return (
+        <Tabs screenOptions={{ headerShown: false }}>
+            {tabs.map((tab) => (
+                <Tabs.Screen 
+                    key={tab.name} 
+                    name={tab.name} 
+                    options={{
+                        tabBarIcon: ({ focused }) => <TabIcon focused={focused} icon={tab.icon} />,
+                        // tabBarShowLabel: false,
+                        // tabBarActiveTintColor: "#007AFF",
+                        // tabBarInactiveTintColor: "#AFB2B6",
+                    }}
+                />
+            ))}
+        </Tabs>
+    )
+}
+    
+
+ 
 
 export default TabLayout
