c696030ca83333dc900aa801e0755f6b4a358c78
vijaymistri1990 <vijaymakwana12790@gmail.com>
2026-09-20 13:44:58 +0530

    Adds a new globas.css file

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -12,15 +12,6 @@ export default function Index() {
       <Text className="text-xl font-sans-bold text-success">
         Love you Shiv-Parvati
       </Text>
-      <Link className="mt-2 px-4 bg-primary p-2" href="/Onboarding">
-        <Text className="text-white text-xl">Go to Onboarding</Text>
-      </Link>
-      <Link className="px-4 mt-2 bg-primary p-2" href="/(auth)/SignIn">
-        <Text className="text-white text-xl">SignIn</Text>
-      </Link>
-      <Link className="px-4 mt-2 bg-primary p-2" href="/(auth)/SignUp">
-        <Text className="text-white text-xl">SignUp</Text>
-      </Link>
     </SafeAreaView>
   );
 }
